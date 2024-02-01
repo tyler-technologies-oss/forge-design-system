7ca53afd9321b207309d372448d6787d7ffa61c2
nickonometry <nick.andrews@tylertech.com>
2024-02-01 08:18:03 -0500

    feat: added more content to the AA app

---
+++ static/accessibility-audits/_app/immutable/chunks/navigation.53cee53e.js
@@ -0,0 +1,1 @@
+import{h as o}from"./singletons.8713eec6.js";const e=o("preload_code"),t=o("on_navigate");export{t as o,e as p};
--- static/accessibility-audits/_app/immutable/chunks/navigation.a32de36b.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./singletons.72491900.js";const e=o("preload_code"),t=o("on_navigate");export{t as o,e as p};
