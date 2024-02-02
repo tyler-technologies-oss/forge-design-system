02e831ee9d273718e5af01bb0663c762e9eed867
nickonometry <nick.andrews@tylertech.com>
2024-02-02 07:44:22 -0500

    feat(aa): added more app data to the AA sub app

--- static/accessibility-audits/_app/immutable/chunks/index.091b6412.js
+++
@@ -1,9 +0,0 @@
-import{a as o,b as e,c as t,I as a}from"./icon.95b4e283.js";import{d as l,e as u,f as w,h as g,g as E,r as N,i as x,s as S}from"./icon.95b4e283.js";import{B as s}from"./base-component-delegate.3fe08071.js";import{d as i}from"./base-component.2bf7bdb1.js";/**
- * @license
- * Copyright 2023 Tyler Technologies, Inc. 
- * License: Apache-2.0
- */class d extends s{constructor(n){super(n)}_build(){return document.createElement(o.elementName)}}/**
- * @license
- * Copyright 2023 Tyler Technologies, Inc. 
- * License: Apache-2.0
- */function p(){i(a)}window&&!window[e]&&(window[e]=t);export{o as ICON_CONSTANTS,e as ICON_REGISTRY_KEY,l as IconAdapter,a as IconComponent,d as IconComponentDelegate,u as IconFoundation,t as IconRegistry,w as awaitIconDefinition,p as defineIconComponent,g as fetchIconContent,E as getCachedIcon,N as removeIconListener,x as sanitizeExternalType,S as sanitizeSvgContent};
--- static/accessibility-audits/_app/immutable/chunks/platform.b72ae179.js
+++
@@ -1,7 +0,0 @@
-/**
- * @license
- * Copyright Google LLC All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */const t=typeof Intl<"u"&&Intl.v8BreakIterator;class e{static get isBrowser(){return typeof document=="object"&&!!document}static get isMobile(){return e.isBrowser&&/Mobi/.test(navigator.userAgent)}static get EDGE(){return e.isBrowser&&/(edge)/i.test(navigator.userAgent)}static get TRIDENT(){return e.isBrowser&&/(msie|trident)/i.test(navigator.userAgent)}static get BLINK(){return e.isBrowser&&!!(window.chrome||t)&&!!CSS&&!e.EDGE&&!e.TRIDENT}static get WEBKIT(){return e.isBrowser&&/AppleWebKit/i.test(navigator.userAgent)&&!e.BLINK&&!e.EDGE&&!e.TRIDENT}static get IOS(){return e.isBrowser&&/iPad|iPhone|iPod/.test(navigator.userAgent)&&!window.MSStream}static get FIREFOX(){return e.isBrowser&&/(firefox|minefield)/i.test(navigator.userAgent)}static get ANDROID(){return e.isBrowser&&/android/i.test(navigator.userAgent)&&!e.TRIDENT}SAFARI(){return e.isBrowser&&/safari/i.test(navigator.userAgent)&&e.WEBKIT}}export{e as P};
