278aa70d8bcbeaf58e1cb2d84907de8ec7bb8ab6
nickonometry <nick.andrews@tylertech.com>
2024-02-02 12:15:51 -0500

    feat: UI site updates and enhancements

--- static/accessibility-audits/_app/immutable/chunks/index.a2241094.js
+++
@@ -1,9 +0,0 @@
-import{a as o,b as e,c as t,I as a}from"./icon.84015d2d.js";import{d as l,e as u,f as w,h as g,g as E,r as N,i as x,s as S}from"./icon.84015d2d.js";import{B as s}from"./base-component-delegate.cf4eafa8.js";import{d as i}from"./base-component.e899f709.js";/**
- * @license
- * Copyright 2023 Tyler Technologies, Inc. 
- * License: Apache-2.0
- */class d extends s{constructor(n){super(n)}_build(){return document.createElement(o.elementName)}}/**
- * @license
- * Copyright 2023 Tyler Technologies, Inc. 
- * License: Apache-2.0
- */function p(){i(a)}window&&!window[e]&&(window[e]=t);export{o as ICON_CONSTANTS,e as ICON_REGISTRY_KEY,l as IconAdapter,a as IconComponent,d as IconComponentDelegate,u as IconFoundation,t as IconRegistry,w as awaitIconDefinition,p as defineIconComponent,g as fetchIconContent,E as getCachedIcon,N as removeIconListener,x as sanitizeExternalType,S as sanitizeSvgContent};
