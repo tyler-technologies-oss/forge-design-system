cfbaafddf625bed0276bca1f1ccafea79fa8d63f
nickonometry <nick.andrews@tylertech.com>
2024-02-21 12:54:10 -0500

    chore: updated copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -75,9 +75,8 @@ function SurveyDialog({ }) {
     </div>
      <div className={styles.surveyDialogRightColumn}>
       <div className={styles.surveyDialogRightColumnInfo}>
-       <h1>We want your feedback!</h1>
-       <p className={styles.fs16}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.  </p>
-       <p className={styles.fs16}>-The Forge Team</p>
+       <h1>We value your feedback!</h1>
+       <p className={styles.fs16}>Help us enhance Forge. Please spare 5 minutes to complete our survey and share your thoughts. Your input is crucial in shaping the future of our design system. Thank you for taking part.</p>
        <div className={styles.formContainer}>
          <a href="#" target="_blank" className={clsx(styles.linkButton, styles.surveyButton, 'button button--primary')}>Take the survey!</a>
          <button className="button button--link" onClick={closeDialog}>Close the dialog</button>
