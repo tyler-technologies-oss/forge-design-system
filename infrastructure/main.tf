d6e910df9ed448a1c45133bcafeb3bc6778cdfc4
raymondgao-tyler <114943152+raymondgao-tyler@users.noreply.github.com>
2025-08-15 01:21:05 -0700

    chore: use root domain

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -150,7 +150,7 @@ module "forge_docs_spa" {
 
   # Domain setup
   route53_root_domain       = var.parent_zone_name
-  route53_subdomains        = [var.domain_name]
+  route53_subdomains        = []
   alias_route53_root_domain = false
 
   # ACM and SSL
